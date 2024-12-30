cf757cf4bab09bed4372a10c8d26bf907abdac7c
AlirezaAB <alirezaabtech@gmail.com>
2024-12-30 12:55:29 +0330

    Add docstring to BookOperations constructor

--- book_operations.py
+++ book_operations.py
@@ -1,4 +1,11 @@
 from book import Book
 class BookOperations:
+    
     def __init__(self):
+        """
+        Initializes the BookOperations object.
+
+        Attributes:
+            books (list): A list to store all the books in the system.
+        """
         self.books = []
