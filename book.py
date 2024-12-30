512e3edfcee8b78124d6ee9dd321e64642b7b044
AlirezaAB <alirezaabtech@gmail.com>
2024-12-30 08:49:02 +0330

    add constructor to Book class with attributes

--- book.py
+++ book.py
@@ -1,2 +1,11 @@
 class Book:
-    pass
+    def __init__(self,title,author,genre,year_of_publication,publisher,page_count,language,price,stock_quantity):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.year_of_publication = year_of_publication
+        self.publisher = publisher
+        self.page_count = page_count
+        self.language = language
+        self.price = price
+        self.stock_quantity = stock_quantity
