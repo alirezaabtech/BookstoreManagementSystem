73471b2e9330ebaf6007db80546e1546950d2d0e
AlirezaAB <alirezaabtech@gmail.com>
2024-12-30 12:36:27 +0330

    Enhance Book class constructor to include id and detailed docstring

--- book.py
+++ book.py
@@ -1,5 +1,30 @@
 class Book:
-    def __init__(self,title,author,genre,year_of_publication,publisher,page_count,language,price,stock_quantity):
+    def __init__(self, 
+             title: str, 
+             author: str, 
+             genre: str, 
+             year_of_publication: int, 
+             publisher: str, 
+             page_count: int, 
+             language: str, 
+             price: float, 
+             stock_quantity: int,
+             id: int) -> None:
+        """
+        Initializes a new Book object.
+
+        Parameters:
+            title (str): The title of the book.
+            author (str): The author of the book.
+            genre (str): The genre of the book.
+            year_of_publication (int): The year the book was published.
+            publisher (str): The publisher of the book.
+            page_count (int): The total number of pages in the book.
+            language (str): The language the book is written in.
+            price (float): The price of the book.
+            stock_quantity (int): The quantity of the book available in stock.
+            id (int): The unique identifier of the book.
+        """    
         self.title = title
         self.author = author
         self.genre = genre
@@ -9,3 +34,4 @@ class Book:
         self.language = language
         self.price = price
         self.stock_quantity = stock_quantity
+        self.id = id
